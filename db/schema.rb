e2b4c02387dd996be2465eec53d29da2218f739d
tak400 <ogttkms@gmail.com>
2020-03-18 23:47:18 +0900

    create image table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_11_111213) do
+ActiveRecord::Schema.define(version: 2020_03_11_144726) do
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_images_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
