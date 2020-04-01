f319adc43c690cff1b6ac4413c0f0e70d17a40e7
tak400 <ogttkms@gmail.com>
2020-04-01 23:46:54 +0900

    create route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "images#index"
   resources :images, only:[:index, :new]
+  get '/images', to: 'images#show'
+  post '/images/create', to: 'images#create'
 end
