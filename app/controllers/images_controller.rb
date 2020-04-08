2494b0296cf544cdcc942280e0c19c04e15848ed
tak400 <ogttkms@gmail.com>
2020-04-08 23:47:56 +0900

    create action

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,2 +1,18 @@
 class ImagesController < ApplicationController
+  def show
+    @images = Image.all
+  end
+
+  def create
+    @image = Image.new(permit_params)
+    @image.save!
+    redirect_to action: 'show'
+  end
+
+  private
+
+  def permit_params
+    params.require(:image).permit(:name, :image)
+  end
+
 end
