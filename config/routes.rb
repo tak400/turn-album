47f85c20ac47c4298e36ab9e481bf8c76220fcb4
tak400 <ogttkms@gmail.com>
2020-04-15 23:56:54 +0900

    change route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "images#index"
-  resources :images, only:[:index, :new]
+  resources :images, only:[:index, :new, :show]
   get '/images', to: 'images#show'
   post '/images/create', to: 'images#create'
 end
